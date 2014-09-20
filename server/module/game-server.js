0b0adace776b5ef98f9a2eb4c3d2216616228396
Bartol Karuza <me@bartolkaruza.com>
2014-09-20 20:22:27 +0200

    added list to gameList response

--- server/module/game-server.js
+++ server/module/game-server.js
@@ -2,7 +2,7 @@
 var io;
 
 var games = {};
-
+var gamesList = [];
 exports.init = function(socketIo) {
     io = socketIo;
     io.sockets.on('connection', function (socket) {
@@ -10,6 +10,7 @@ exports.init = function(socketIo) {
             console.log(message);
         });
         socket.on('measurement', function (measurement) {
+            var game = games[measurement.gameId];
             socket.broadcast.emit('update', {currentColor:"afrdbn", targetColor:"afrdbn"});
         });
     });
@@ -21,13 +22,13 @@ exports.init = function(socketIo) {
 
 exports.handleCreateGame = function(req, resp) {
     var game = req.body;
-    games[game.name] = ({name: game.name, values:[game.deviceAddress], status:'waiting'});
+    games[game.name] = ({name: game.name, values:[{address:game.deviceAddress, playerNumber:1}], status:'waiting', targetColor:getRandomColor()});
+    gamesList.push(games[game.name]);
     resp.send(200, games[req.body.name]);
-//    resp.send(200, game);
 };
 
 exports.handleGameList = function(req, resp) {
-    resp.send(200, games);
+    resp.send(200, gamesList);
 };
 
 exports.handleGetGame = function(req, resp) {
@@ -38,7 +39,27 @@ exports.handleGetGame = function(req, resp) {
 exports.handleJoin = function(req, resp) {
     var id = req.params['id'];
     var game = games[id];
-    game.values.push(req.body.deviceAddress);
+    game.values.push({address:req.body.deviceAddress, playerNumber:(game.values.length + 1)});
     if(game.values.length > 2) game.status = 'started';
     resp.send(200, game);
 };
+
+var currentColor = {
+    red:0,
+    green:0,
+    blue:0
+};
+
+function updateMeasurement(measurement) {
+    var game = games[measurement.gameId];
+
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
