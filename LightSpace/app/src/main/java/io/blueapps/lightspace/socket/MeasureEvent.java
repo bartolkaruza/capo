bc88d5e53649b264c2a10bd24c690685bed0a576
Bartol Karuza <me@bartolkaruza.com>
2014-09-20 16:52:38 +0200

    added two way socket connections

--- LightSpace/app/src/main/java/io/blueapps/lightspace/socket/MeasureEvent.java
+++ LightSpace/app/src/main/java/io/blueapps/lightspace/socket/MeasureEvent.java
@@ -1,8 +1,20 @@
 package io.blueapps.lightspace.socket;
 
+import android.util.Pair;
+
+import java.util.List;
+
 /**
  * Created by bartolkaruza on 20/05/14.
  */
 public class MeasureEvent {
+
+    private String deviceId;
+    private List<Pair<String, Integer>> measurements;
+
+    public MeasureEvent(String deviceId, List<Pair<String, Integer>> measurements) {
+        this.deviceId = deviceId;
+        this.measurements = measurements;
+    }
 
 }
